d7436414a37391744b5a9f34de739330559f1b3f
joq62 <joakim.leche@gmail.com>
2019-03-19 22:26:15 +0100

    dns_data added service_info

--- dns_data.hrl
+++ dns_data.hrl
@@ -1,6 +1,27 @@
 %% service_info
 %% Information of the servie
 
+-define(HEARTBEAT_INTERVAL,1*20*1000).
+-define(INACITIVITY_TIMEOUT,1*60*1000).
+-define(DNS_TABLE,dns_table).
+
+-define(DNS_INFO(Zone,ServiceId,IpAddr,Port,Time,Schedule),{Zone,ServiceId,IpAddr,Port,Time,Schedule}).
+-define(DNS_INFO_ZONE(DnsInfo),element(1,hd([DnsInfo]))).
+-define(DNS_INFO_SERVICE(DnsInfo),element(2,hd([DnsInfo]))).
+-define(DNS_INFO_IPADDR(DnsInfo),element(3,hd([DnsInfo]))).
+-define(DNS_INFO_PORT(DnsInfo),element(4,hd([DnsInfo]))).
+-define(DNS_INFO_TIME(DnsInfo),element(5,hd([DnsInfo]))).
+-define(DNS_INFO_SCHEDULE(DnsInfo),element(6,hd([DnsInfo]))).
+
+
+% ServiceInfo used by kubelet
+
+-define(SERVICE_INFO(Zone,ServiceId,IpAddr,Port),{Zone,ServiceId,IpAddr,Port}).
+-define(SERVICE_INFO_ZONE(ServiceInfo),element(1,hd([ServiceInfo]))).
+-define(SERVICE_INFO_SERVICE(ServiceInfo),element(2,hd([ServiceInfo]))).
+-define(SERVICE_INFO_IPADDR(ServiceInfo),element(3,hd([ServiceInfo]))).
+-define(SERVICE_INFO_PORT(ServiceInfo),element(4,hd([ServiceInfo]))).
+
 -record (dns_info, 
          {
 	   time_stamp="not_initiaded_time_stamp",    % un_loaded, started
